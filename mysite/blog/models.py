ba55c1bd2e3cd9225a1a9e1cdfd772c812f33993
Doan Bao <younglafire@gmail.com>
2026-06-10 10:23:29 +0700

    Implement pagination for blog posts and update templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -52,7 +52,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
-    def get_absolute_url(self): 
+    def get_absolute_url(self):
         return reverse(
             'blog:post_detail', # return the url in urls.py, then can be used globally
             args=[
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,21 @@
-from django.http import Http404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
-    
+    post_list = Post.published.all()
+# Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1) # get the page, default 1
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # If page number is out of range, get the last page of results
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # Get the first page
+        posts = paginator.page(1)
     return render(
         request,
         'blog/post/list.html',
